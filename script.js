1649206c867c7f91c4d07dcd6cc1e54e5523e4ab
Badsha012 <badshasheikh361@gmail.com>
2025-09-03 23:41:54 +0600

    ten commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const loadLessons=() =>{
+    fetch("https://openapi.programming-hero.com/api/levels/all") //promise
+    .then(res => res.json()) //promise of json
+    .then((json) => displayLessons(json.data));
+};
+
+const displayLessons=(lessons)=>{
+    // 1.get the container & empty
+    const levelContainer=document.getElementById("level-container");
+    levelContainer.innerHTML="";
+
+    // 2.get evey element
+    for(let lesson of lessons){
+
+
+        // 3.crate a element
+        const  btnDiv=document.createElement("div");
+        btnDiv.innerHTML=`    <button class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+        </button>
+        `;
+ 
+
+    // 4.append into container
+    levelContainer.append(btnDiv)
+
+    }
+    
+
+
+};
+
+loadLessons();
